0efc8620fd7c9ac68f627df65176760284373e87
zxrkwlxn <cc.zerkhwilson.salisi@cvsu.edu.ph>
2024-11-21 22:35:37 +0800

    update registration

--- frontend/src/registration/Registration.jsx
+++ frontend/src/registration/Registration.jsx
@@ -1,121 +1,52 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button'; // Import the Button component
 
-export default function FormPropsTextFields() {
+export default function App() {
   return (
     <Box
-      component="form"
-      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-      noValidate
-      autoComplete="off"
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        backgroundColor: '#ffffff',
+      }}
     >
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />
-        <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          slotProps={{
-            input: {
-              readOnly: true,
-            },
-          }}
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          slotProps={{
-            inputLabel: {
-              shrink: true,
-            },
-          }}
-        />
-        <TextField id="outlined-search" label="Search field" type="search" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          variant="filled"
-          slotProps={{
-            input: {
-              readOnly: true,
-            },
-          }}
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          variant="filled"
-          slotProps={{
-            inputLabel: {
-              shrink: true,
-            },
-          }}
-        />
-        <TextField
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
-      </div>
+      <Card sx={{ 
+        minWidth: 1920, 
+        maxWidth: '80%', 
+        width: '50%',  
+        height: 400, 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignSelf: 'center', 
+        padding: 4,  
+        gap: 2, 
+        margin: 'auto', 
+        boxShadow: 'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px', 
+      }}> 
+        <CardContent>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '100%', 
+            }}
+          >
+            <TextField required id="outlined-block-no" label="Block No." sx={{ m: 1, width: '25ch' }} />
+            <TextField required id="outlined-lot-no" label="Lot No." sx={{ m: 1, width: '25ch' }} />
+            <TextField id="outlined-phone-no" label="Phone No." type="tel" sx={{ m: 1, width: '25ch' }} />
+            <TextField id="outlined-email" label="Email" type="email" sx={{ m: 1, width: '25ch' }} />
 
+            <Button variant="contained" sx={{ mt: 2 }}>Save</Button> {/* Added the Save button */}
+          </Box>
+        </CardContent>
+      </Card>
     </Box>
   );
 }
