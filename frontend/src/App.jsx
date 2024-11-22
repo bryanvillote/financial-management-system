a502f3fc575d4de377afc1adf9187fca0b5b3f6a
Jchael <abbas.johnmichael.eccbsc@gmail.com>
2024-11-23 00:18:51 +0800

    fix imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,14 @@
-{/*import Dashboard from "./dashboard/Dashboard"
-import Welcome from "./welcome/Welcome"*/}
-import LogIn from "./auth/LogIn"
-
 
 function App() {
 
   return (
     <>
-      {/*<Welcome/>
-      {/*<Dashboard />*/}
-      <LogIn/>
+      {/* <SignIn /> */}
+      {/*<Dashboard/>*/}
+      {/*<Welcome/>*/}
+      {/*<LogIn/>*/}
     </>
   )
 }
 
 export default App
-
