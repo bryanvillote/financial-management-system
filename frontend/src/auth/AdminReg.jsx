619b4d772ccf3be207fd9e962256c86ce177be59
Jchael <abbas.johnmichael.eccbsc@gmail.com>
2025-01-13 17:42:55 +0800

    adjustments

--- frontend/src/auth/AdminReg.jsx
+++ frontend/src/auth/AdminReg.jsx
@@ -33,11 +33,27 @@ const StyledCard = styled(Card)(({ theme }) => ({
 }));
 
 const SignInContainer = styled(Stack)(({ theme }) => ({
-  height: "100vh",
+  height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
+  width: "100vw",
+  minHeight: "100%",
   padding: theme.spacing(2),
+  backgroundImage: `linear-gradient(to right, #020140, transparent), url('/loginbg.png')`,
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
   [theme.breakpoints.up("sm")]: {
     padding: theme.spacing(4),
   },
+"&::before": {
+  content: '""',
+  display: "block",
+  position: "absolute",
+  zIndex: -1,
+  ...theme.applyStyles("dark", {
+    backgroundImage:
+      "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+  }),
+},
 }));
 
 export default function AdminReg(props) {
